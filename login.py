7c5886eb848d3bd05cf4acf743267a7a8ae8ce2e
Isaac Holt <holtimzws@outlook.com>
2022-02-24 11:43:06 -0700

    Pushed code

---
+++ login.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+
+class Log_in:
+    driver = webdriver.Firefox()
+
+    def __init__(self):
+        self.driver = ()
+    
+    def DriverStart(self):
+        self.driver = webdriver.Firefox()
+# Old example code
+'''    bob = str()
+
+    age = int()
+
+
+    def __init__(
+            self, 
+            newBob = "Bob the Builder", 
+            newAge = 10000
+        ):
+        
+        self.bob = newBob
+        self.age = newAge
+    
+    def GetAge(self):
+        return self.age'''
