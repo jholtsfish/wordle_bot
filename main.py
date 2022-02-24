cc171c18b70c1f0af11b6c72e8438b4aa1e05093
Joseph Holt <jholtsfish@gmail.com>
2022-02-24 11:43:05 -0700

    filtering list to only 5 letter words

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 from login import Login
+
+thing = Login()
+
+with open("valid_words.txt") as file:
+    # words = file.readlines()
+    # words = [word.strip() for word in words]
+    words = [word for word in (word.strip() for word in file.readlines()) if len(word) == 5]
+
+    file.close()
+
+print(words)
+print(thing)
