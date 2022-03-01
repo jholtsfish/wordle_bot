88ba7ab157e8a64bbec3a7b42758690b0cd8c040
Isaac Holt <holtimzws@outlook.com>
2022-02-28 18:17:00 -0700

    Changes

--- userInfo.py
+++ userInfo.py
@@ -1,5 +1,6 @@
 import json
 from cryptography.fernet import Fernet
+import pickle
 
 class user:
     userName = None
@@ -11,61 +12,68 @@ class user:
     def __init__(self):
         try:
             # Loading json files into the program
-            with open('userNames.json') as data:
+            with open('userNames.json', 'rb') as data:
                 self.userName = json.load(data)
-            with open('userPasswords.json') as data:
+            with open('userPasswords.json', 'rb') as data:
                 self.userPassword = json.load(data)
-            with open('userNamesKey.json') as data:
+            with open('userNamesKey.json', 'rb') as data:
                 self.encNameKey = json.load(data)  
-            with open('userPasswordsKey.json') as data:
-                self.userPasswordKey = json.load(data)          
+            with open('userPasswordsKey.json', 'rb') as data:
+                self.encPasswordKey = json.load(data)
+            self.ogUserName = self.userName
+            self.ogUserPassword = self.userPassword
+            self.ogEncNameKey = self.encNameKey
+            self.ogEncPasswordKey = self.encPasswordKey   
         except FileNotFoundError:
             self.userName = {}
             self.userPassword = {}
-            self.userNameKey = {}
+            self.encNameKey = {}
             self.userPasswordKey = {}
-            pass
-        except json.JSONDecodeError:
-            self.userName = {}
-            self.userPassword = {}
-            self.userNameKey = {}
-            self.userPasswordKey = {}
+            self.ogUserName = {}
+            self.ogUserPassword = {}
+            self.ogEncNameKey = {}
+            self.ogEncPasswordKey = {}
             pass
         # End loading json files into the program
-    def CheckUser(self, userName, userPassword):
-        if userName + " username" in self.userName and userName + " password" in self.userPassword:
+    def CheckUser(self, name, pwd):
+        if name + " username" in self.userName and name + " password" in self.userPassword:
             # Decrypting the data in the json files
-            self.userNameCrypt = self.userName[userName + " username"]
-            self.encNameKey = self.userNameKey[userName + " username key"]
+            self.userNameCrypt = self.userName[name + " username"]
+            self.encNameKey = self.encNameKey[name + " username key"]
             self.userName = self.encNameKey.decrypt(self.userNameCrypt).decode()
-            self.userPasswordCrypt = self.userPassword[userName + " password"]
-            self.encPasswordKey = self.userPasswordKey[userName + " password key"]
+            self.userPasswordCrypt = self.userPassword[name + " password"]
+            self.encPasswordKey = self.userPasswordKey[name + " password key"]
             self.userPassword = self.encPasswordKey.decrypt(self.userPasswordCrypt).decode()
             # End decryption of data in the json files
-            return self.userName, self.userPassword
+            return name, pwd
         else:
             # Encryption
             self.key = Fernet.generate_key()
             self.key2 = Fernet.generate_key()
             self.keyUser = Fernet(self.key)
             self.keyPwd = Fernet(self.key2)
-            self.encName = self.keyUser.encrypt(userName.encode())
-            self.encPassword = self.keyPwd.encrypt(userPassword.encode())
+            self.encName = self.keyUser.encrypt(name.encode())
+            self.encPassword = self.keyPwd.encrypt(pwd.encode())
             # End encryption
             # Store in a dictionary
-            self.userName = {f"{userName} username": str(self.encName)}
-            self.userPassword = {f'{userName} password': str(self.encPassword)}
-            self.encNameKey = {f'{userName} username key': str(self.keyUser)}
-            self.encPasswordKey = {f'{userName} password key': str(self.keyPwd)}
+            self.userName = {f"{name} username": str(self.encName)}
+            self.userPassword = {f'{name} password': str(self.encPassword)}
+            self.encNameKey = {f'{name} username key': str(self.keyUser)}
+            self.encPasswordKey = {f'{name} password key': str(self.keyPwd)}
             # End dictionary storage
+            self.userName.update(self.ogUserName)
+            self.userPassword.update(self.ogUserPassword)
+            self.encNameKey.update(self.ogEncNameKey)
+            self.encPasswordKey.update(self.ogEncPasswordKey)
+            print(self.userName, self.userPassword, self.encNameKey, self.encPasswordKey)
             # Save to json files
             with open('userNames.json', 'w') as data:
-                json.dump(self.userName, data, indent=4, ensure_ascii = False)
+                json.dump(self.userName, data)
             with open('userPasswords.json', 'w') as data:
-                json.dump(self.userPassword, data, indent=4, ensure_ascii = False)
+                json.dump(self.userPassword, data)
             with open('userNamesKey.json', 'w') as data:
-                json.dump(self.encNameKey, data, indent=4, ensure_ascii = False)
+                json.dump(self.encNameKey, data)
             with open('userPasswordsKey.json', 'w') as data:
-                json.dump(self.encPasswordKey, data, indent=4, ensure_ascii = False)
+                json.dump(self.encPasswordKey, data)
             # End saving to json files
-            return self.userName, self.userPassword
+            return name, pwd
