38f4922811baaece36c99d84a149d3902dc71a95
Isaac Holt <holtimzws@outlook.com>
2022-03-02 13:08:52 -0700

    Make the code much nicer and easier to read

--- userInfo.py
+++ userInfo.py
@@ -5,15 +5,24 @@ import pickle
 import sys
 import time
 class user:
+
+
+    # Declare any vars needed
     userName = None
     userPassword = None
     encName = None
     encPassword = None
     encNameKey = None
     encPasswordKey = None
+    # Done declaring
+
+
     def __init__(self):
+
+
+    # Loading pickle files into the program. We load them first so we have any data needed.  
         try:
-            # Loading json files into the program
+
             with open('userNames.pkl', 'rb') as data:
                 self.userName = pickle.load(data)
             with open('userPasswords.pkl', 'rb') as data:
@@ -22,10 +31,17 @@ class user:
                 self.encNameKey = pickle.load(data)  
             with open('userPasswordsKey.pkl', 'rb') as data:
                 self.encPasswordKey = pickle.load(data)
+
+
+            # Declare vars that we add to the dictionary later to add multi-user support
             self.ogUserName = self.userName
             self.ogUserPassword = self.userPassword
             self.ogEncNameKey = self.encNameKey
-            self.ogEncPasswordKey = self.encPasswordKey   
+            self.ogEncPasswordKey = self.encPasswordKey 
+            # End declaring
+
+
+        # If the pickle files don't exist, we make sure the program doesn't crash  
         except FileNotFoundError:
             self.userName = {}
             self.userPassword = {}
@@ -35,8 +51,29 @@ class user:
             self.ogUserPassword = {}
             self.ogEncNameKey = {}
             self.ogEncPasswordKey = {}
+            error = "File not found"
             pass
+
+        except:
+            self.userName = {}
+            self.userPassword = {}
+            self.encNameKey = {}
+            self.userPasswordKey = {}
+            self.ogUserName = {}
+            self.ogUserPassword = {}
+            self.ogEncNameKey = {}
+            self.ogEncPasswordKey = {}
+            otherError = "Unknown error"
+            pass
+        # End crash handling
+
+
+    # End loading pickle files
+
+
+    # Check user funtion 1: Checks if a user exists. 2: Loads the user if it does exist. 3: If the user doesn't exist, it calls CreateUser to create a user.
     def CheckUser(self, name, pwd):
+
         # Decrypting the data in the json files
         try:
             self.userNameCrypt = self.userName[name + " username"]
@@ -46,10 +83,15 @@ class user:
             self.encPasswordKey = self.encPasswordKey[name + " password key"]
             self.userPassword = self.encPasswordKey.decrypt(self.userPasswordCrypt).decode()
         except:
+            # This makes sure that if there isn't any keys, or if the user doesn't exist in the pickle file, the program will continue and not crash
             pass
-        # End decryption of data in the json files   
+        # End decryption of data in the json files 
+
+
+        # Check if the username and password match up with the ones in the pickle file  
         for i in range(5):
             if self.userName == name and self.userPassword == pwd:
+                # These rightPassword variables enhance the process
                 rightPassword = True
                 break
             elif self.userName == name and self.userPassword != pwd:
@@ -57,25 +99,40 @@ class user:
                 rightPassword = False
                 continue
             else:
+                # Create a user if one doesn't exist. 
                 createUser = str.lower(str(input("Sorry! That user doesn't exist! Create a new user?\ny/n -->")))
                 if createUser == 'y':
                     self.CreateUser(name, pwd)
                     rightPassword = True
                     break
+                # End creating a user
+
+
+                # If the user doesn't want to create an account
                 else:
                     rightPassword = False
+                    # Not create enhances the process of not making a user
                     notCreate = True
                     print("Okay. We won't save your details.")
                     break
+                # End not wanting an account
+
         if rightPassword:
+            # Return vars needed for the program
             return name, pwd
         elif notCreate:
+            # Return vars needed for the program
             return name, pwd
         else:
             print("Sorry! Your password didn't match our database, so we'll have to exit.\nTry running the program again.")
             time.sleep(3)
             sys.exit()
+        # End checking if the user exists, and if they typed the right password
+
+
+    # Funtion to encrypt, and store variables to create a user
     def CreateUser(self, name, pwd):
+
         # Encryption
         self.key = Fernet.generate_key()
         self.key2 = Fernet.generate_key()
@@ -84,18 +141,26 @@ class user:
         self.encName = self.keyUser.encrypt(name.encode())
         self.encPassword = self.keyPwd.encrypt(pwd.encode())
         # End encryption
+
+
         # Store in a dictionary
         self.userName = {f"{name} username": self.encName}
         self.userPassword = {f'{name} password': self.encPassword}
         self.encNameKey = {f'{name} username key': self.keyUser}
         self.encPasswordKey = {f'{name} password key': self.keyPwd}
         # End dictionary storage
+
+
+        # Update the dictionaries for multi-user support
         self.userName.update(self.ogUserName)
         self.userPassword.update(self.ogUserPassword)
         self.encNameKey.update(self.ogEncNameKey)
         self.encPasswordKey.update(self.ogEncPasswordKey)
         print(self.userName, self.userPassword, self.encNameKey, self.encPasswordKey)
-        # Save to json files
+        # End updating
+
+
+        # Save to pickle files
         with open('userNames.pkl', 'wb') as data:
             pickle.dump(self.userName, data)
         with open('userPasswords.pkl', 'wb') as data:
@@ -104,6 +169,8 @@ class user:
             pickle.dump(self.encNameKey, data)
         with open('userPasswordsKey.pkl', 'wb') as data:
             pickle.dump(self.encPasswordKey, data)
-        # End saving to json files
+        # End saving to pickle files
+
+
+        # Return the vars needed for the program
         return name, pwd
-        # End loading json files into the program
