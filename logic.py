5bb6e993d916dfe0b8bddb994eec478570cd4617
Dom Kirnite <conner@darlingappraising.com>
2022-03-05 14:38:55 -0700

    lol

--- logic.py
+++ logic.py
@@ -1,1 +1,14 @@
- 
+class wordlogic:
+    def __init__(self):
+        good = 'good'
+    def sortwords(self, filepath):
+        wordfile = open(filepath, 'r')
+        wordlines = wordfile.readlines()
+        for line in wordlines:
+            line.strip()
+            print(line)
+
+
+
+
+
--- login.py
+++ login.py
@@ -1,49 +1,8 @@
 # Why is this necessary? Wordle doesn't have a login. What is this for????
 
-'''from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-import time
+#lol using this file to test stuff now ~conner
 
-class Log_in:
+from logic import wordlogic
 
-# No vars needed for this one
+wordlogic.sortwords('valid_words.txt')
 
-# Init vars
-    def __init__(self):
-        self.EMAILFIELD = (By.ID, "i0116")
-        self.PASSWORDFIELD = (By.ID, "i0118")
-        self.NEXTBUTTON = (By.ID, "idSIButton9")
-        self.SIGNINBUTTON = (By.ID, "id_a")
-# End init vars
-
-
-# Define a function that logs into wordle
-    def WordleLogin(self, driver, name, pwd):
-        # Go to wordle website
-        driver.get('https://login.live.com')
-
-
-        # Wait for email field and enter email
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable(self.EMAILFIELD)).send_keys(name)
-
-
-        # Click Next
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable(self.NEXTBUTTON)).click()
-
-
-        # Wait for password field and enter password
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable(self.PASSWORDFIELD)).send_keys(pwd)
-
-
-        # Click Login
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable(self.NEXTBUTTON)).click()
-
-
-        # Click Yes
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable(self.NEXTBUTTON)).click()
-
-
-        # Wait a bit
-        time.sleep(1)
-# End defining'''
