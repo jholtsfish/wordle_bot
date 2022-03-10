14fb96176fcd7f34cc1648c52ad90f036749ed67
Joseph Holt <jholtsfish@gmail.com>
2022-03-10 14:57:10 -0700

    outsourced finding elements to js script

---
+++ access_elements.js
@@ -0,0 +1,4 @@
+console.log(
+    document.getElementsByTagName("game-app")
+    .shadowRoot()
+    )
--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-from login import Login
 from init import WORDS
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 
-thing = Login()
+# install webdriver service manager
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+driver.get("https://www.nytimes.com/games/wordle")
+
+# use js script to access shadow elements
+js = open("./access_elements.js")
+print(
+    driver.execute_script(js.read())
+)
+js.close()
+
+input()
