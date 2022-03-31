f3fc9b8854bcbca2a540b25e5df92c963b2da5e4
Isaac Holt <holtimzws@outlook.com>
2022-03-30 19:27:27 -0600

    Delete __init__.py

--- __init__.py
+++
@@ -1,7 +0,0 @@
-with open("valid_words.txt") as file:
-    WORDS = []
-    for word in file.readlines():
-        word = word.strip()
-        if len(word) == 5:
-            WORDS.append(word)
-    file.close()
