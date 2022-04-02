1913e93f44662056486acca15348fc21d47f6d07
Dom Kirnite <conner@darlingappraising.com>
2022-04-02 16:47:59 -0600

    new words to use

--- logic.py
+++ logic.py
@@ -1,59 +1,74 @@
 class wordlogic:
     def __init__(self):
-        gkjdfsksdfkjgsd = 'yo'
+        nothing = ''
+        
+    def filterwords(self, unfilteredfile, writefile):
+        with open(unfilteredfile, 'r') as wordfile:
+
+            wordlines = wordfile.readlines()
+
+            for i in range(len(worddlines) -1, -1, -1):
+                        wordlines[i] = wordlines[i].strip()
+                        if len(wordlines[i]) != 5:
+                            print(f"removing word: {wordlines[i]}")
+                            wordlines.remove(wordlines[i])
+
+        with open(writefile, 'w') as f:
+            f.writelines("%s\n" % l for l in wordlines)
+
     def sortwords(self, filepath):
+        #read the txt file containing words, remove all non 5 letter words and then put them in a list
         wordfile = open(filepath, 'r')
         wordlines = wordfile.readlines()
         for i in range(len(wordlines) -1, -1, -1):
             wordlines[i] = wordlines[i].strip()
             if len(wordlines[i]) != 5:
                 wordlines.remove(wordlines[i])
+
+        #dicts for each index in the 5 letter word, keys are the letters of the alphabet, values are how many times the letter was found in the dicts index. (changed to a new value later)
         letters1 = {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0, 'k': 0, 'l': 0, 'm': 0, 'n': 0 , 'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0, 'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0}
-
         letters2 = {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0, 'k': 0, 'l': 0, 'm': 0, 'n': 0 , 'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0, 'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0}
-
         letters3 = {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0, 'k': 0, 'l': 0, 'm': 0, 'n': 0 , 'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0, 'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0}
-
         letters4 = {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0, 'k': 0, 'l': 0, 'm': 0, 'n': 0 , 'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0, 'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0}
-
         letters5 = {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0, 'k': 0, 'l': 0, 'm': 0, 'n': 0 , 'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0, 'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0}
-        optimword = {1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e'}
+        dictlist = [letters1, letters2, letters3, letters4, letters5]
+        wordlinesdict = {}
+
+        #adding +1 to the value of letters every time they are found in a certain index in a word
         for line in wordlines:
             letters1[line[0]] += 1
             letters2[line[1]] += 1
             letters3[line[2]] += 1
             letters4[line[3]] += 1
             letters5[line[4]] += 1
-        l1 = []
-        l2 = []
-        l3 = []
-        l4 = []
-        l5 = []
-        for i in letters1:
-            l1.append(letters1[i])
-        for i in letters2:
-            l2.append(letters2[i])
-        for i in letters3:
-            l3.append(letters3[i])
-        for i in letters4:
-            l4.append(letters4[i])
-        for i in letters5:
-            l5.append(letters5[i])
-        l1 = sorted(l1)
-        l2 = sorted(l2)
-        l3 = sorted(l3)
-        l4 = sorted(l4)
-        l5 = sorted(l5)
-        let1best = 1
-        let1top = 'a'
-        dict1sum = 0
-        dict2sum = 0 
-        dict3sum = 0 
-        dict4sum = 0 
-        dict5sum = 0
+        
+        #convert dict values from how many times a letter appears, to the % of the time that it appears (%chance that the letter will appear)
+        for dict in dictlist:
+            for letter in dict:
+                dict[letter] = float(dict[letter]) / float(12653) 
 
-        sumlist = [dict1sum, dict2sum, dict3sum, dict4sum, dict5sum]
-        dictlist = [letters1, letters2, letters3, letters4, letters5]
+        #add every word to a dictionary, then assign a value to it based on the %chance for each letter that i just did above
+        for word in wordlines:
+            wordlinesdict[word] = float(letters1[word[0]]) * float(letters2[word[1]]) * float(letters3[word[2]]) * float(letters4[word[3]]) * float(letters5[word[4]]) * 100000000
+
+        #sort that dictionary
+        self.sortedwords = sorted(wordlinesdict.items(), key = lambda kv: kv[1])
+        
+    
+    def guess(self, previousword, letter1color, letter2color, letter3color, letter4color, letter5color):
+        #colors can be y(ellow), g(reen), n(ull, or grey)
+        print('.')
+
+
+        
+        
+
+        
+        
+        
+            
+
+    
 
        
             
---
+++ run.py
@@ -0,0 +1,12 @@
+#file to run stuff in logic.py
+
+from logic import wordlogic
+
+wordlogic = wordlogic()
+
+wordlogic.sortwords('2words.txt')
+
+print(wordlogic.sortedwords)
+
+
+
