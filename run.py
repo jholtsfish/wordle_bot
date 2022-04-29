a94c182932d8b3ede8f24440e148246734b7a8b4
Dom Kirnite <conner@darlingappraising.com>
2022-04-29 16:36:45 -0600

    improve run.py

--- run.py
+++ run.py
@@ -18,7 +18,9 @@ list4 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
         ]
 list5 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
         'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-        ]
+]       
+for i in range(500):
+    print("\n")
 
 wordfile = input("use custom wordlist? [default: none] --> ")
 if '.' not in wordfile:
